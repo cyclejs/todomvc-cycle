d483316271c293ff3bc9dc16cce35d0a2557f74d
Andre Medeiros <andre.medeiros@futurice.com>
2014-11-13 23:14:19 +0200

    Refactored InitialTodosModel

--- js/models/initial-todos.js
+++ js/models/initial-todos.js
@@ -1,30 +1,28 @@
 'use strict';
 /*global Cycle */
 
+function merge() {
+	var result = {};
+	for (var i = 0; i < arguments.length; i++) {
+		var object = arguments[i];
+		for (var key in object) {
+			if (object.hasOwnProperty(key)) {
+				result[key] = object[key];
+			}
+		}
+	}
+	return result;
+}
+
 var InitialTodosModel = Cycle.defineModel(function () {
-	var storedTodosData = localStorage.getItem('todos-cycle');
 	var defaultTodosData = {
 		list: [],
 		input: '',
 		filter: '',
 		filterFn: function () { return true; } // allow anything
 	};
-	var initialTodosData = storedTodosData ?
-		JSON.parse(storedTodosData) :
-		defaultTodosData;
-	// TODO replace this with merge objects function
-	if (typeof initialTodosData.list === 'undefined') {
-		initialTodosData.list = defaultTodosData.list;
-	}
-	if (typeof initialTodosData.input === 'undefined') {
-		initialTodosData.input = defaultTodosData.input;
-	}
-	if (typeof initialTodosData.filter === 'undefined') {
-		initialTodosData.filter = defaultTodosData.filter;
-	}
-	if (typeof initialTodosData.filterFn === 'undefined') {
-		initialTodosData.filterFn = defaultTodosData.filterFn;
-	}
+	var storedTodosData = JSON.parse(localStorage.getItem('todos-cycle')) || {};
+	var initialTodosData = merge(defaultTodosData, storedTodosData);
 	return {
 		todosData$: Rx.Observable.just(initialTodosData)
 	};
