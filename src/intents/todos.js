d7b0d685cafb21f7f0cc7bb9cd466fb43ffde71b
Andre Medeiros <andre.medeiros@futurice.com>
2015-07-17 22:57:39 +0300

    Isolate window/hashchange event stream as a driver

--- src/intents/todos.js
+++ src/intents/todos.js
@@ -1,30 +1,30 @@
 import {Rx} from '@cycle/core';
 import {ENTER_KEY, ESC_KEY} from '../utils';
 
-export default function intent(domDriver) {
+export default function intent(DOM, hashchange) {
   return {
-    changeRoute$: Rx.Observable.fromEvent(window, 'hashchange')
+    changeRoute$: hashchange
       .map(ev => ev.newURL.match(/\#[^\#]*$/)[0].replace('#', ''))
       .startWith(window.location.hash.replace('#', '')),
 
-    clearInput$: domDriver.get('#new-todo', 'keyup')
+    clearInput$: DOM.get('#new-todo', 'keyup')
       .filter(ev => ev.keyCode === ESC_KEY),
 
-    insertTodo$: domDriver.get('#new-todo', 'keyup')
+    insertTodo$: DOM.get('#new-todo', 'keyup')
       .filter(ev => {
         let trimmedVal = String(ev.target.value).trim();
         return ev.keyCode === ENTER_KEY && trimmedVal;
       })
       .map(ev => String(ev.target.value).trim()),
 
-    editTodo$: domDriver.get('.todo-item', 'newContent').map(ev => ev.detail),
+    editTodo$: DOM.get('.todo-item', 'newContent').map(ev => ev.detail),
 
-    toggleTodo$: domDriver.get('.todo-item', 'toggle').map(ev => ev.detail),
+    toggleTodo$: DOM.get('.todo-item', 'toggle').map(ev => ev.detail),
 
-    toggleAll$: domDriver.get('#toggle-all', 'click'),
+    toggleAll$: DOM.get('#toggle-all', 'click'),
 
-    deleteTodo$: domDriver.get('.todo-item', 'destroy').map(ev => ev.detail),
+    deleteTodo$: DOM.get('.todo-item', 'destroy').map(ev => ev.detail),
 
-    deleteCompleteds$: domDriver.get('#clear-completed', 'click')
+    deleteCompleteds$: DOM.get('#clear-completed', 'click')
   };
 };
