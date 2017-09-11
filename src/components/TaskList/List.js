c472209718b4211cf126fd8bb3277807dddbb96f
Andre Staltz <andre@staltz.com>
2017-09-12 00:33:54 +0300

    Update to use onionify rc.17

--- src/components/TaskList/List.js
+++ src/components/TaskList/List.js
@@ -1,22 +1,14 @@
 import {ul} from '@cycle/dom';
-import {pickCombine, pickMerge} from 'cycle-onionify';
+import {makeCollection} from 'cycle-onionify';
 import Task from '../Task/index';
 
-export function List(sources) {
-  const tasks = sources.onion.toCollection(Task)
-    .uniqueBy(s => s.key)
-    .isolateEach(key => key)
-    .build(sources);
-
-  const vdom$ = tasks
-    .pickCombine('DOM')
-    .map(vnodes => ul('.todo-list', vnodes));
-
-  const reducer$ = tasks
-    .pickMerge('onion');
-
-  return {
-    DOM: vdom$,
-    onion: reducer$,
-  };
-}
+export const List = makeCollection({
+  item: Task,
+  itemKey: state => state.key,
+  itemScope: key => key,
+  collectSinks: instances => ({
+    DOM: instances.pickCombine('DOM')
+      .map(vnodes => ul('.todo-list', vnodes)),
+    onion: instances.pickMerge('onion'),
+  })
+});
